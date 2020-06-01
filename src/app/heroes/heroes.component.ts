2d873961532c6fa91d04c3ef05fc80e24bc37c67
Atarius Armstrong <atariusarmstrong@gmail.com>
2020-06-01 11:15:54 -0400

    created heroe list and added private styling for heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero'
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -14,9 +15,13 @@ export class HeroesComponent implements OnInit {
     name: "Windstorm"
   } ;
 
+  heroes = HEROES;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+
